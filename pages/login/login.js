9cefee55af96f6b1e163fe5f86de64271082a766
xiaofan <1353815633@qq.com>
2018-09-02 12:23:27 +0800

    帐号密码输入

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,66 +1,36 @@
 // pages/login/login.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
+    disabled: true,
+    btnstate: 'default',
+    account: '',
+    password: ''
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
+  accountInput: function (event) {
+    var content = event.detail.value.trim();
+    console.log(content);
+    if (content !== '') {
+      this.setData({
+        disabled: false, 
+        btnstate: 'primary', 
+        account: content
+      });
+    } else {
+      this.setData({
+        disabled: true,
+        btnstate: 'default'
+      });
+    }
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  pwdBlur: function(e) {
+    var password = e.detail.value;
+    if (password != '') {
+      this.setData({
+        password: password
+      });
+    }
   }
+
 })
